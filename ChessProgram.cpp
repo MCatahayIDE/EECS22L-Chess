d73351af290a551811beb63976921ff8bdc8cf71
MCatahayIDE <mcatahay@gmail.com>
2025-04-09 20:36:33 -0700

    Commit Message

---
+++ ChessProgram.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+
+//object class definitions
+class piece{};
+
+class Player{};
+
+class ChessBoard{};
+
+class ChessGame{};                             //object initializes a chess game
+
+//position class, an attribute of each piece to tracks its location on the board
+
+//GUI class, generates the board representation UI
+
+//Game Ending Conditions
+
+//program will start an instance of a chess game and initiate play
+int main() {
+    ChessGame gameInstance;
+    
+}
+
